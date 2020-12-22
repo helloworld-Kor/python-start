8e3f2ee6dde5613120e150b46e6f3883814f8e56
Helloworld <koteakang77@naver.com>
2020-12-23 01:53:07 +0900

    빗썸 공지사항 크롤링 중

--- selenium/listsTest.py
+++ selenium/listsTest.py
@@ -32,41 +32,28 @@ def upbit():
             
         time.sleep(30)   
     return results
-
-def junggo(keyword):
+def bithumb():
+    last_id = "" 
+    cnt = 0
     while True:
-        ccnt = 1
-        keyword = re.sub(" ", "%20", keyword)  # &od=1
-        url = "https://apis.naver.com/cafe-web/cafe-search-api/v1.0/trade-search/all?query={}&page=1&size=30".format(
-            keyword, ccnt)
-        req = requests.get(url)
-        _json = req.json()
-        lists = _json.get("result").get("tradeArticleList")
-        jungo = []
-        # pprint.pprint(lists[0])
-        articleId = "https://cafe.naver.com/joonggonara/"
-        articleId += str(lists[0].get("articleId"))
-        writeTime = lists[0].get("writeTime") / 1000
-        post_id =lists[0].get("articleId")
-        li = lists[0].get("productSale")
-        cost = li.get("cost")
-        product = li.get("productName")
-        stats = li.get("saleStatus")
-        juso = ""
-        s = datetime.fromtimestamp(writeTime)
-        for region in li.get("regionList"):
-            # cnt += 1
-            juso = str(region['regionName1'])
-            juso += " " + str(region['regionName2'])
-            juso += " "+str(region['regionName3'])
+        cnt += 1 
+        # results = []
+        url = "https://cafe.bithumb.com/view/boards/43"
+        r = requests.get(url)
+        bs = BeautifulSoup(r.text, "lxml")
+        links = bs.select("#dataTables > tbody > tr:nth-child(5) > td.invisible-mobile.small-size")
+        link = links[0].text
+        results = bs.select("#dataTables > tbody > tr:nth-child(5) > td.one-line > a")
+        result = results[0].text
+        print(link)
+        print(result)
+        post_id = link
+        if last_id != post_id:
+            last_id = post_id
+            print("새로운글 {}".format(result))
+            URL = 'https://api.telegram.org/bot1480467875:AAFQsa0P93WmXfQ1airEjmXcSexzmrZHc-E/sendMessage?chat_id=1440556547%20&text={}'.format(result)
+            requests.get(URL)
         
-        jungo.append((post_id, str(s), cost, product, stats,juso,  articleId))
-        
-
-        URL = 'https://api.telegram.org/bot1480467875:AAFQsa0P93WmXfQ1airEjmXcSexzmrZHc-E/sendMessage?chat_id=1440556547%20&text={}'.format(jungo)
-        response = requests.get(URL)
-        time.sleep(10)
-
-    return jungo
-
-upbit()
+        time.sleep(10)   
+    # return results
+bithumb()
